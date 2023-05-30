180043605df0d27149f64fdac59143a5f85f1c3d
David Fuhry <david.fuhry@uni-leipzig.de>
2023-05-30 15:36:28 +0200

    Make disc to roll transformation work with color images

--- src/hmsm/cardboard_discs.py
+++ src/hmsm/cardboard_discs.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
+import logging
 from skimage.measure import EllipseModel, label
 from scipy.spatial import distance
-from typing import Optional
-from hmsm.utils import read_as_binary_image, crop_image_to_contents, morphological_edge_detection
+from typing import Optional, Tuple
+from hmsm.utils import read_as_binary_image, crop_image_to_contents, morphological_edge_detection, binarize_image, interpolate_missing_pixels
 
 # For debugging only
 import matplotlib.pyplot as plt
@@ -12,22 +13,91 @@ import matplotlib.pyplot as plt
 def process_disc():
     pass
 
-def transform_to_rectangle(input_image: np.ndarray, offset: Optional[int] = 0) -> np.ndarray:
+def transform_to_rectangle(image: np.ndarray, offset: Optional[int] = 0, binarize: Optional[bool] = False) -> np.ndarray:
     """Transforms an image of a circular music storage medium to the shape of a rectangular one
 
     Args:
-        image (np.ndarray): The image to be transformed, is expected to be a binary image as returned by :func:`~hmsm.utils.read_as_binary_image`
+        image (np.ndarray): The image to be transformed, can be a binary image as returned by :func:`~hmsm.utils.read_as_binary_image` or just an image as read by skimage.imread
         offset (Optional[int], optional): The offset (in degrees, counterclockwise) of the beginning of the disc. Defaults to 0.
+        binarize (Optional[bool], optional): Weather the output image should be binarized.
 
     Returns:
         np.ndarray: Image of the transformed medium
     """
-    # Read image and apply preprocessing
-    
-    image = crop_image_to_contents(input_image.copy())
-    edges = morphological_edge_detection(image)
+    # Apply preprocessing
 
+    if binarize:
+        image = binarize_image(image)
+
+    image = crop_image_to_contents(image.copy())
+
+    logging.info("Determening disc measurements and center")
+
+    center_x, center_y, a, b, theta = fit_ellipse_to_circumference(image)
+
+    # Create a mask for all pixels that are within the disc
+    ellipse_mask = cv2.ellipse(np.zeros((image.shape[0], image.shape[1]), np.uint8), (center_x, center_y), (int(a), int(b)), theta, 0, 360, 1, -1)
+
+    # Right now the disc background is filled while the holes are empty
+    # This should be fine, but does significantly increase computational demand
+    if binarize:
+        image = np.invert(image)
+
+    logging.info("Extracting location information for all pixels")
+
+    # Get all points that are within the disc
+
+    coords = np.argwhere(ellipse_mask == 1)
+
+    values = image[ellipse_mask == 1]
+
+    # Shift points around the center to make the center be (0,0)
+
+    coords = coords - np.array([center_x, center_y])
+
+
+    # Calculate position in degrees
+
+    degrees = np.arctan2(coords[:,1], coords[:,0]) * 180 / np.pi
+    degrees = ((np.round(degrees, decimals = 1) + 180) * 10).astype(np.int16)
+
+    # Apply offset if applicable
+
+    if not offset == 0:
+        logging.info("Applying offset")
+        degrees = degrees - (offset * 10)
+        degrees[degrees < 0] = degrees[degrees < 0] + 3600
+
+    degrees = degrees.astype(np.uint16)
+
+    # Calculate distances
+
+    dists = np.round(distance.cdist(np.array([[0, 0]]), coords)[0], decimals = 0).astype(np.uint16)
+
+    # Build 2d image
+
+    logging.info("Applying calculated transformations and creating output image")
+
+    dims = (3601, dists.max() + 10) if values.ndim == 1 else (3601, dists.max() + 10, values.shape[1])
+    image_rect = np.zeros(dims, np.uint8)
+    image_rect[degrees, dists] = values
+
+    logging.info("Interpolating missing pixels in output image")
+
+    mask = np.full((image_rect.shape[0], image_rect.shape[1]), True, bool)
+    mask[degrees, dists] = False
+
+    interpolated_image = interpolate_missing_pixels(image_rect, mask)
+
+    return interpolated_image
+
+def fit_ellipse_to_circumference(image: np.ndarray) -> Tuple[int, int, int, int, float]:
+    if image.ndim == 3 or np.unique(image).size > 2:
+        image = binarize_image(image)
+    
     # Label the image to find the outer edge of the disc
+
+    edges = morphological_edge_detection(image)
 
     labels = label(edges, background = 0, connectivity = 2)
 
@@ -36,7 +106,6 @@ def transform_to_rectangle(input_image: np.ndarray, offset: Optional[int] = 0) -
     edge = np.argwhere(labels == 1)
 
     # Fit an ellipse to the outer edge to determine the image center
-    # TODO: Refactor into helper function
 
     ell = EllipseModel()
     ell.estimate(edge)
@@ -46,48 +115,4 @@ def transform_to_rectangle(input_image: np.ndarray, offset: Optional[int] = 0) -
     a = int(a)
     b = int(b)
 
-    # Create a mask for all pixels that are within the disc
-    ellipse_mask = cv2.ellipse(np.zeros_like(image), (center_x, center_y), (int(a), int(b)), theta, 0, 360, 1, -1)
-
-    image = np.invert(image)
-
-    image[ellipse_mask == 0] = 0
-
-    points_filled = np.argwhere(image == 255)
-
-    # Shift points around the center to make the center be (0,0)
-
-    points_filled = points_filled - np.array([center_x, center_y])
-
-    # Calculate position in degrees
-
-    degrees_filled = np.arctan2(points_filled[:,1], points_filled[:,0]) * 180 / np.pi
-    degrees_filled = ((np.round(degrees_filled, decimals = 1) + 180) * 10).astype(np.int16)
-
-    # Apply offset if applicable
-
-    if not offset == 0:
-        degrees_filled = degrees_filled - (offset * 10)
-        degrees_filled[degrees_filled < 0] = degrees_filled[degrees_filled < 0] + 3600
-
-    degrees_filled = degrees_filled.astype(np.uint16)
-
-    # Calculate distances
-
-    dists_filled = np.round(distance.cdist(np.array([[0, 0]]), points_filled)[0], decimals = 0).astype(np.uint16)
-
-    # Convert into triplets
-
-    triplets = np.column_stack((degrees_filled, dists_filled, np.full(dists_filled.shape, 1)))
-
-    # Build 2d image
-
-    image_rect = np.zeros((3601, dists_filled.max() + 10), np.uint8)
-    image_rect[triplets[:,0], triplets[:,1]] = triplets[:,2]
-
-    # Fill holes
-
-    kernel = np.ones((3,3),np.uint8)
-    image_rect = cv2.morphologyEx(image_rect, cv2.MORPH_CLOSE, kernel)
-
-    return image_rect
+    return (center_x, center_y, a, b, theta)
--- src/hmsm/utils.py
+++ src/hmsm/utils.py
@@ -8,30 +8,42 @@ from skimage.filters import threshold_otsu
 from skimage.color import rgb2gray
 from skimage.io import imread
 from skimage import img_as_ubyte
+from scipy import interpolate
 
 def read_as_binary_image(path: str, threshold: Optional[int] = None) -> np.ndarray:
-    if not os.path.isfile(path):
-        logging.error(f"The system could not find the specified file at path '{path}', aborting")
-        sys.exit()
+    try:
+        img = imread(path)
+    except FileNotFoundError:
+        logging.error(f"The system could not find the specified file at path '{path}', could not read image file")
+        raise
 
     img = imread(path)
 
     logging.info(f"Image read from '{path}'")
 
-    img = rgb2gray(img)
+    return binarize_image(img)
 
+def binarize_image(image: np.ndarray, threshold: Optional[int] = None) -> np.ndarray:
+    if image.shape[2] == 3:
+        image = rgb2gray(image)
+    
     if threshold is None:
-        threshold = threshold_otsu(img)
-        logging.info(f"No threshold value was specified for binarization, used Otsu's method to estimate threshold to be {threshold * 255}")
+        threshold = threshold_otsu(image)
     else:
         threshold = (threshold / 255)
 
-    img_bin = img > threshold
+    img_bin = image > threshold
     img_bin = np.invert(img_bin)
 
     return img_as_ubyte(img_bin)
 
 def crop_image_to_contents(image: np.ndarray) -> np.ndarray:
+    output_image = image.copy()
+
+    # Binarize the image to make sure we don't get stiffled by some weird gray values
+    if image.ndim == 3 or np.unique(image).size > 2:
+        image = binarize_image(image)
+
     y_values, x_values = np.nonzero(image)
 
     y_min = y_values.min() - 20 if y_values.min() - 20 >= 0 else 0
@@ -40,7 +52,7 @@ def crop_image_to_contents(image: np.ndarray) -> np.ndarray:
     x_min = x_values.min() - 20 if x_values.min() - 20 >= 0 else 0
     x_max = x_values.max() + 20 if x_values.max() + 20 <= image.shape[1] - 1 else image.shape[1] - 1
 
-    cropped_image = image.copy()[y_min:y_max, x_min:x_max]
+    cropped_image = output_image[y_min:y_max, x_min:x_max]
     return cropped_image
 
 def morphological_edge_detection(image: np.ndarray, n_erosions: Optional[int] = 2) -> np.ndarray:
@@ -54,3 +66,15 @@ def morphological_edge_detection(image: np.ndarray, n_erosions: Optional[int] = 
     edges = image - image_eroded
 
     return edges
+
+def interpolate_missing_pixels(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    unknown_coords = np.argwhere(mask == True)
+
+    interpolated_values = interpolate.griddata(
+        np.argwhere(mask == False), image[mask == False], unknown_coords,
+        method="nearest", fill_value= [0,0,0]
+    )
+
+    interpolated_image = image.copy()
+    interpolated_image[unknown_coords[:,0], unknown_coords[:,1]] = interpolated_values
+    return interpolated_image
